31b4cb053fa5de73785a99cef2035c69ea2cbbff
pelinbasaslan <pelin.basaslan@gmail.com>
2024-12-17 16:29:25 +0300

    Patika'ya neden başladım

---
+++ PatikaIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Pelin Su Başaslan");
+Console.WriteLine("Patika Bootcamp'i, sağladığı disiplinli çalışma düzeni ve kapsamlı içerikleri nedeniyle tercih ettim."
++ "Her seviyeye uygun dersler, sektördeki uzmanlar tarafından hazırlanan eğitim materyalleri ve etkili bir öğrenme süreci sunarak, bu alandaki bilgi ve becerilerimi hızla geliştirmemi sağlayacağına inanıyorum." 
++ "Ayrıca, verilen geri bildirimler ve proje odaklı çalışma fırsatları, öğrendiklerimi pratikte uygulamamı kolaylaştıracağı için tercih ettim.");
+    }
+}
